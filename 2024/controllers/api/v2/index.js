a7f60df118f6de4949412200b5ee95e307f05c20
Peter Cardillo <pcardillo@streamingturtles.com>
2024-02-20 14:43:44 -0500

    updated with last additions for updating swagger v2 - adding page selection option in swagger v2

--- 2024/controllers/api/v2/index.js
+++ 2024/controllers/api/v2/index.js
@@ -3,6 +3,18 @@ const categoryRoutes = require('./categories');
 const productRoutes = require('./products');
 const reviewRoutes = require('./reviews');
 
+
+// router.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//       res.set('Cache-Control', 'private, max-age=300');
+//     }
+  
+//     next();
+//   });
+  
+
+
+
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
 router.use('/reviews', reviewRoutes);
